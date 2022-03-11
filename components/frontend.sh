252cf71144809e2a054584435fa523e26ace19c3
Saurabh Mahajan <srbhmahajan61@live.com>
2022-03-11 18:54:15 -0500

    roboshop 1st commit front end updated

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,1 +1,29 @@
+#! /usr/bin/bash
+
 echo frontend component
+
+
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+
+#Let's download the HTDOCS content and deploy under the Nginx pat#
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+#Deploy in Nginx Default Location.
+
+
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+
+#Finally restart the service once to effect the changes.
+
+systemctl restart nginx
+systemctl enable nginx
