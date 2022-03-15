3b1e8514890fea141447c93627664eb203cfd19f
Saurabh Mahajan <srbhmahajan61@live.com>
2022-03-15 18:56:02 -0400

    conditions with number test and file tests

--- conditions.sh
+++ conditions.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/bash
 
-# case and if are conditional cmds, if commands is widely used because it has more options than case `command`
+# case and if are conditional commands, if commands is widely used because it has more options than case `command`
 # if conditions are of 3 types
-#1:if []; then cmds fi
-#2: if []; then cmds else cmds fi
-#3:if []; then cmd elif []; then cmds else cmds fi
+#1:if []; then commands fi
+#2: if []; then commands else command fi
+#3:if []; then cmd elif []; then commands else commands fi
 # if is found in in 3forms
 # 1. spring tests
 a="abc"
@@ -26,5 +26,10 @@ fi
 
 
 # operators : ==, !=, -z
-#2. mumber tests
+#2. number tests
+#operators : -eq, -ne, -le, -lt, -gt, -ge
 #3. file tests
+#-e -> returns true if file exists
+# -f returns true if file is a file not a directory
+# -d returns true if a file is a directory
+#-r, -w, -x returns true if file has read, wrote or execute permissions for user running the file
