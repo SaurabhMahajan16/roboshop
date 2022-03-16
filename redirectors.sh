7f545b7cf08756889f549ed1073d70bd69db67bf
Saurabh Mahajan <srbhmahajan61@live.com>
2022-03-16 14:59:16 -0400

    enabled curl command

--- redirectors.sh
+++ redirectors.sh
@@ -1,25 +1,25 @@
 #! /usr/bin/bash
 
-ls - l &>/dev/null
+ls - l &> /dev/null
 #this wont store the result as this file is used for nullifying the o/p
 touch /tmp/abc
-ls -l >/tmp/abc
+ls -l > /tmp/abc
 #redirecting o/p of ls -l to a file
 cd roboshop
-ls -l >/tmp/abc
+ls -l > /tmp/abc
 #cat /tmp/abc
 #this will override the data in the abc file
 #in order to append data in file we have to use >>
 cd
-ls - l >>/tmp/abc
+ls - l >> /tmp/abc
 #cat /tmp/abc
 cd roboshop
-ls -l >>/tmp/abc
+ls -l >> /tmp/abc
 #cat /tmp/abc
 #now this redirector will append the data in the file
 
 #output and error des not redirect to same file unless specified
 #in order to redirect both o/p & error to same file use &>
 touch /tmp/def
-ls -l &>/tmp/def
+ls -l &> /tmp/def
 #cat /tmp/def
