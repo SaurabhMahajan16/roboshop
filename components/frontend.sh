8aa19a322def75261021e6b2b566387dea62a5e8
Saurabh Mahajan <srbhmahajan61@live.com>
2022-03-16 12:29:19 -0400

    enabled curl command

--- components/frontend.sh
+++ components/frontend.sh
@@ -3,9 +3,19 @@
 
 
 Print(){
-  echo -e "\e[34m \n\n $1 \n\n \e[0m"
+  echo -e "\e[34m \n\n ${1} \n\n \e[0m"
 }
+exitStatusCheck(){
+ if [ "${1}" -eq 0 ]; then
+ # echo -e "\e[32m nginx installed successfully\n\n\n \e[0m"
+  Print "${2} Successfully"
+ else
+   Print "${2} failed"
+ #  echo -e "\e[32m nginx installed failed\e[0m"
+   exit 2
+ fi
 
+}
 Print "frontend component"
 
 # checking for root user permissions
@@ -19,72 +29,86 @@ fi
 Print "installing nginx"
 #echo -e "\e[32m installing nginx \e[0m"
 yum install nginx -y
-
-if [ $? -eq 0 ]; then
+exitStatusCheck $? " Nginx installed -"
+#if [ $? -eq 0 ]; then
 # echo -e "\e[32m nginx installed successfully\n\n\n \e[0m"
- Print "nginx installed successfully"
-else
-  Print "nginx installed failed"
+# Print "nginx installed successfully"
+#else
+#  Print "nginx installed failed"
 #  echo -e "\e[32m nginx installed failed\e[0m"
-  exit 2
-fi
+#  exit 2
+#fi
 
 #Let's download the HTDOCS content and deploy under the Nginx path
 Print "download the HTDOCS content and deploy under the Nginx path"
 #echo -e "\e[32m \n\n download the HTDOCS content and deploy under the Nginx path \e[0m"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 # curl -f will change the exit if the download fails otherwise curl wont give an exit error code
-if [ $? -eq 0 ]; then
-  Print " download the HTDOCS content successful"
+exitStatusCheck $? "download HTDOCS content -"
+#if [ $? -eq 0 ]; then
+#  Print " download the HTDOCS content successful"
 # echo -e "\e[32m nginx installed successfully\n\n\n \e[0m"
-else
-  Print " download the HTDOCS content failure"
+#else
+#  Print " download the HTDOCS content failure"
 #  echo -e "\e[32m nginx installed failed\e[0m"
-  exit 2
-fi
+#  exit 2
+#fi
 
 #Deploy in Nginx Default Location.
 
-Print "clean old nginx location &  Deploy in Nginx Default Location"
+Print "clean old nginx location "
 #echo -e "\e[32m \n\n\n clean old nginx location &  Deploy in Nginx Default Location \e[0m"
 cd /usr/share/nginx/html
 rm -rf *
+exitStatusCheck $? "clean old nginx -"
+
+Print "extracting archive"
 unzip /tmp/frontend.zip
+exitStatusCheck $? "extracted -"
+
+Print "moving to current working directory"
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
+exitStatusCheck $? "moving to current - "
+
+
+Print "Deploy in Nginx Default Location"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-if [ $? -eq 0 ]; then
+
+
   Print " cleaning and deployed successfully"
-# echo -e "\e[32m nginx installed successfully\n\n\n \e[0m"
-else
-  Print " cleaning or deployed failure"
-#  echo -e "\e[32m nginx installed failed\e[0m"
-  exit 2
-fi
+#if [ $? -eq 0 ]; then
+## echo -e "\e[32m nginx installed successfully\n\n\n \e[0m"
+#else
+#  Print " cleaning or deployed failure"
+##  echo -e "\e[32m nginx installed failed\e[0m"
+#  exit 2
+#fi
 
 #Finally restart the service once to effect the changes.
 Print "restart and enable the service to effect the changes"
 #echo -e "\e[32m restart and enable the service to effect the changes \e[0m"
 systemctl restart nginx
-
-if [ $? -eq 0 ]; then
-  Print " nginx restarted successfully "
-# echo -e "\e[32m nginx installed successfully\n\n\n \e[0m"
-else
-  Print " nginx restart failure "
-#  echo -e "\e[32m nginx installed failed\e[0m"
-  exit 2
-fi
+exitStatusCheck $? "restart nginx - "
+#if [ $? -eq 0 ]; then
+#  Print " nginx restarted successfully "
+## echo -e "\e[32m nginx installed successfully\n\n\n \e[0m"
+#else
+#  Print " nginx restart failure "
+##  echo -e "\e[32m nginx installed failed\e[0m"
+#  exit 2
+#fi
 
 systemctl enable nginx
-
-if [ $? -eq 0 ]; then
-  Print " nginx enabled successfully "
-# echo -e "\e[32m nginx installed successfully\n\n\n \e[0m"
-else
-  Print " nginx enabled failure "
-#  echo -e "\e[32m nginx installed failed\e[0m"
-  exit 2
-fi
+exitStatusCheck $? "enabled nginx"
+#if [ $? -eq 0 ]; then
+#  Print " nginx enabled successfully "
+## echo -e "\e[32m nginx installed successfully\n\n\n \e[0m"
+#else
+#  Print " nginx enabled failure "
+##  echo -e "\e[32m ngi
+# nx installed failed\e[0m"
+#  exit 2
+#fi
