0785be4623005ed9df92404aeeee76de16da1c60
Saurabh Mahajan <srbhmahajan61@live.com>
2022-03-16 14:52:51 -0400

    enabled curl command

--- redirectors.sh
+++ redirectors.sh
@@ -7,18 +7,18 @@ ls -l >/tmp/abc
 #redirecting o/p of ls -l to a file
 cd /roboshop
 ls -l >/tmp/abc
-cat /tmp/abc
+#cat /tmp/abc
 #this will override the data in the abc file
 #in order to append data in file we have to use >>
 cd
 ls - l >>/temp/abc
-cat /tmp/abc
+#cat /tmp/abc
 cd /roboshop
 ls -l >>/tmp/abc
-cat /tmp/abc
+#cat /tmp/abc
 #now this redirector will append the data in the file
 
 #output and error des not redirect to same file unless specified
 #in order to redirect both o/p & error to same file use &>
 ls -l &>/tmp/def
-cat /tmp/def
+#cat /tmp/def
