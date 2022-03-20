f9a7548e7115267696c75c26a095b4bf5fc53fd8
Saurabh Mahajan <srbhmahajan61@live.com>
2022-03-19 23:05:33 -0400

    mysql service with proper coding practices except password keeping in file

--- components/mysql.sh
+++ components/mysql.sh
@@ -19,16 +19,17 @@ systemctl enable mysqld &>>"${logFile}" && systemctl start mysqld &>>"${logFile}
 exitStatusCheck $?
 
 #1. Now a default root password will be generated and given in the log file.
-echo "show databases" | mysql -uroot -pRoboShop@1 &>>"${logFile}"
+
 #in mysql we dont need to give spaces between options and inputs like in -uroot -u is option and root is input slrly -p is for passwd and Roboshop@1 is password
 #we use condition because if we are running script again & again the the password is already changed so we cant change it again and again so if condition is checking whether password is changed by validating the password
+echo "show databases" | mysql -uroot -pRoboShop@1 &>>"${logFile}"
 if [ $? -ne 0 ]; then
   Print "changing default root password for mysql"
   echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('Roboshop@1');" >/tmp/rootpass.sql
-  defaultRootPassword=$(grep "temporary password" /var/log/mysqld.log | awk "{print $NF}")
+  defaultRootPassword=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
   #here we used round brackets because we are assigning a variable a command
 #Nth field means last field but if u want 2nd last NF-1 or similar
-  mysql --connect-expired-password -uroot -p"${defaultRootPassword}" </tmp/rootpass.sql &>>"${logFile}"
+  mysql --connect-expired-password -uroot -p${defaultRootPassword} </tmp/rootpass.sql &>>"${logFile}"
   #we use --connect-expired-password as default password is expired so we have to got with this so we have to use this option
   exitStatusCheck $?
 fi
