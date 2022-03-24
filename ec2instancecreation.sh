3114ac88ed289de8034bb1513184a4440cb9bb66
Saurabh Mahajan <srbhmahajan61@live.com>
2022-03-24 00:35:43 -0400

    ec2 creation and adding security group and spot instance

--- ec2instancecreation.sh
+++ ec2instancecreation.sh
@@ -41,7 +41,7 @@ Print "${AmiId}"
 
 Print "getting security group Id"
 Print "${securityGroupInput}"
-SecurityGroupId=(aws ec2 describe-security-groups --filters Name=group-name,Values=$securityGroupInput | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g') &>>"${logFile}"
+SecurityGroupId=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${securityGroupInput} | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g') &>>"${logFile}"
 exitStatusCheck $?
 Print "${SecurityGroupId}"
 
